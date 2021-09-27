c21b67e3a34a994b00261bd4948cea92ab3d6985
Lucas Hoffman <75821031+lucash0331@users.noreply.github.com>
2021-09-26 22:00:18 -0400

    Update app.js

--- week-6/ems/app.js
+++ week-6/ems/app.js
@@ -1,36 +1,68 @@
 /*
-Title: Exercise 6.4 - Web 340
-Author: Lucas Hoffman
-Date: 09/19/2021
-Description: App.js file for assignment 6.4
+=================================================================
+;  Title: app.js
+;  Author: Lucas Hoffman
+;  Date: September 26, 2021
+;  Description: App.js Code for EMS Project
+=================================================================
 */
 
-// Set requires for express, http, path and logger 
+//Require statements for express, http, path and morgan.
 var express = require("express");
 var http = require("http");
 var path = require("path");
 var logger = require("morgan");
+var mongoose = require("mongoose");
+var Employee = require("./models/employee");
+var mongoDB = "mongodb+srv://admin:admin@buwebdev-cluster-1.umga8.mongodb.net/test";
 
+mongoose.connect(mongoDB, {});
+
+mongoose.Promise = global.Promise;
+
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error: "));
+
+db.once("open", function () {
+  console.log("Application connected to MongoDB instance");
+});
+
+// Assigning express object to app variable
 var app = express();
 
-// Set views location and EJS as view engine
+//Telling express to look in the views folder for files.
 app.set("views", path.resolve(__dirname, "views"));
+
+//Telling express to use the ejs view engine
 app.set("view engine", "ejs");
 
-// Morgan options
-app.use(logger("short"));
+//Adding the logger app
+app.use(logger("dev"));
 
-// Set view for rooot dir
+//Get request for the index page
 app.get("/", function (request, response) {
   response.render("index", {
-    title: "Home page",
+    title: "Home Page",
   });
 });
 
-// Include static folder for CSS file for this assignment
-app.use(express.static(__dirname + "/css"));
+//Get request for the list page
+app.get("/list", function (request, response) {
+  response.render("list", {
+    title: "Lists Page",
+  });
+});
 
-// Start webserver on port 8000
-http.createServer(app).listen(8000, function () {
-  console.log("Application started on port 8000!");
+//Get request for the new employee page
+app.get("/new", function (request, response) {
+  response.render("new", {
+    title: "New Employee Page",
+  });
+});
+
+//Creates server to listen on port 8080
+http.createServer(app).listen(8080, function () {
+  console.log("Application started on port 8080!");
+});
 });
